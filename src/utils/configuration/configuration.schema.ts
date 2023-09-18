9bd9e3c3c39c8b867877646ff10652a455a963d9
Nils Müller <tyriis@users.noreply.github.com>
2023-09-18 23:36:02 +0200

    feat(configuration): add redis sentinel configuration

--- src/utils/configuration/configuration.schema.ts
+++ src/utils/configuration/configuration.schema.ts
@@ -18,14 +18,22 @@ export const configValidationSchema: Joi.ObjectSchema = Joi.object().keys({
     port: Joi.number().port().default(3000),
     pretty: Joi.boolean().default(false),
   }).unknown(false),
-  redis: Joi.object({
-    host: Joi.string()
-      .ip({
-        version: ["ipv4"],
-        cidr: "forbidden",
-      })
-      .default("127.0.0.1"),
-    port: Joi.number().port().default(6379),
-    keyPrefix: Joi.string().optional(),
-  }),
+  redis: Joi.object()
+    .keys({
+      host: Joi.string().hostname(),
+      port: Joi.number().port(),
+      keyPrefix: Joi.string().optional(),
+      sentinels: Joi.array().items(
+        Joi.object().keys({
+          host: Joi.string().hostname(),
+          port: Joi.number().port().default(26379),
+        }),
+      ),
+      name: Joi.string(),
+    })
+    .xor("host", "sentinels")
+    .xor("port", "sentinels")
+    .xor("host", "name")
+    .xor("port", "name")
+    .and("sentinels", "name"),
 })
