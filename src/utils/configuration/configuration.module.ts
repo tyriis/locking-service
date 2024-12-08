db91722ed8a273148c60532e9ca358b73b255325
Nils Müller <tyriis@users.noreply.github.com>
2024-12-08 22:07:53 +0100

    feat(config): adjust config module to work with latest nest version

--- src/utils/configuration/configuration.module.ts
+++ src/utils/configuration/configuration.module.ts
@@ -1,13 +1,16 @@
-import { ConfigModule } from "@nestjs/config"
-import { DynamicModule } from "@nestjs/common"
-import { configurationLoader } from "./configuration-loader"
-import { envValidationSchema } from "./configuration.schema"
+import { ConfigModule } from "@nestjs/config";
+import { DynamicModule } from "@nestjs/common";
+import { configurationLoader } from "./configuration-loader";
+import { envValidationSchema } from "./configuration.schema";
 
-export const configurationModule: DynamicModule = ConfigModule.forRoot({
-  cache: true,
-  load: [configurationLoader],
-  validationSchema: envValidationSchema,
-  validationOptions: {
-    abortEarly: true,
-  },
-})
+export const configurationModule: DynamicModule = {
+  module: ConfigModule,
+  ...ConfigModule.forRoot({
+    cache: true,
+    load: [configurationLoader],
+    validationSchema: envValidationSchema,
+    validationOptions: {
+      abortEarly: true,
+    },
+  })
+};
